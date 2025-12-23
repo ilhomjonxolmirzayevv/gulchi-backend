a5599b964e6191010890e01e942bc15ce1ab57a5
ilhomjonxolmirzayevv <146935614+ilhomjonxolmirzayevv@users.noreply.github.com>
2025-12-23 18:24:39 +0500

    Update midilweris.js

--- start/midilweris.js
+++ start/midilweris.js
@@ -4,21 +4,17 @@ const cors = require('cors');
 const path = require('path');
 
 module.exports = function (app) {
-    // ! Middleware
+    // Enable CORS for all origins
+    app.use(cors());
+
+    app.use(helmet());
     app.use(express.json());
 
-    // ! Built in middleware
+    // Static files
     app.use(express.static('public'));
     app.use(express.static('uploads'));
     app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
     app.use('/caruselImg', express.static(path.join(__dirname, '../caruselImg')));
 
-    // ! Third party middleware
-    app.use(helmet());
-    if (app.get('env') === 'development') {
-        app.use(cors());
-    }
-
-    // ! View engine
     app.set('view engine', 'ejs');
 };
